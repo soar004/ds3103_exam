4ab5fb05d8c713227a4b65c9adf50d445a301894
Solveig Årvik <soar004@student.kristiania.no>
2023-11-14 19:21:09 +0100

    added comments

--- Prosjektmapper/Formula1API/Controllers/TeamsController.cs
+++ Prosjektmapper/Formula1API/Controllers/TeamsController.cs
@@ -16,67 +16,82 @@ public class TeamsController : ControllerBase
         _context = context;
     }
 
+    // GET: api/teams
     [HttpGet]
     public ActionResult<IEnumerable<Team>> GetTeams()
     {
         try
         {
+            // Retrieve all teams from the database
             var teams = _context.Teams.ToList();
             return Ok(teams);
         }
         catch (Exception ex)
         {
+            // Handle internal server error
             return StatusCode(500, $"Internal server error: {ex.Message}");
         }
     }
 
+    // GET: api/teams/{id}
     [HttpGet("{id}")]
     public ActionResult<Team> GetTeamById(int id)
     {
         try
         {
+            // Retrieve a team by ID from the database
             var team = _context.Teams.FirstOrDefault(t => t.Id == id);
 
             if (team == null)
             {
+                // Return 404 Not Found if the team is not found
                 return NotFound();
             }
             return Ok(team);
         }
         catch (Exception ex)
         {
+            // Handle internal server error
             return StatusCode(500, $"Internal server error: {ex.Message}");
         }
     }
 
+    // POST: api/teams
     [HttpPost]
     public ActionResult<Team> CreateTeam([FromBody] Team newTeam)
     {
         try
         {
+            // Add a new team to the database
             _context.Teams.Add(newTeam);
             _context.SaveChanges();
 
+            // Return 201 Created with the newly created team
             return CreatedAtAction(nameof(GetTeamById), new { id = newTeam.Id }, newTeam);
         }
         catch (Exception ex)
         {
+            // Handle internal server error
             return StatusCode(500, $"Internal server error: {ex.Message}");
         }
     }
 
+    // PUT: api/teams/{id}
     [HttpPut("{id}")]
     public ActionResult<Team> UpdateTeam(int id, [FromBody] Team updatedTeam)
     {
         try
         {
+            // Retrieve the existing team from the database
             var existingTeam = _context.Teams.FirstOrDefault(t => t.Id == id);
 
             if (existingTeam == null)
             {
+                // Return 404 Not Found if the team is not found
                 return NotFound();
             }
 
+            // Update the existing team in the database
             existingTeam.Manufacturer = updatedTeam.Manufacturer;
             existingTeam.ImgCar = updatedTeam.ImgCar;
             existingTeam.Driver1 = updatedTeam.Driver1;
@@ -84,33 +99,41 @@ public class TeamsController : ControllerBase
 
             _context.SaveChanges();
 
+            // Return the updated team
             return Ok(existingTeam);
         }
         catch (Exception ex)
         {
+            // Handle internal server error
             return StatusCode(500, $"Internal server error: {ex.Message}");
         }
     }
 
+    // DELETE: api/teams/{id}
     [HttpDelete("{id}")]
     public IActionResult DeleteTeam(int id)
     {
         try
         {
+            // Retrieve the team to be removed from the database
             var teamToRemove = _context.Teams.FirstOrDefault(t => t.Id == id);
 
             if (teamToRemove == null)
             {
+                // Return 404 Not Found if the team is not found
                 return NotFound();
             }
 
+            // Remove the team from the database
             _context.Teams.Remove(teamToRemove);
             _context.SaveChanges();
 
+            // Return 204 No Content after successful deletion
             return NoContent();
         }
         catch (Exception ex)
         {
+            // Handle internal server error
             return StatusCode(500, $"Internal server error: {ex.Message}");
         }
     }
