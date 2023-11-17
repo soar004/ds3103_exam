07ecdd763044a8e2e056972ee3f5945a4d92e203
Eline Madsø <elinemadso@gmail.com>
2023-11-17 15:13:36 +0100

    Starting on formula1Context

--- Prosjektmapper/frontend/src/App.tsx
+++ Prosjektmapper/frontend/src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { BrowserRouter, Routes, Route,Link } from 'react-router-dom'
+import { HomePage, DriverPage, NewDriverPage, QuizPage } from './pages'
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <nav>
+          <ul><li><Link to=""></Link></li></ul>
+          <ul><li></li></ul>
+        </nav>
+        <main>
+          <Routes>
+            <Route></Route>
+          </Routes>
+        </main>
+      </BrowserRouter>
     </>
   )
 }
---
+++ Prosjektmapper/frontend/src/context/Formula1Context.tsx
@@ -0,0 +1,4 @@
+//import {useState, createContext} from 'react'
+
+//export const Formula1Context = createContext();
+
