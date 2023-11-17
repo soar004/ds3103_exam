8aeaed63a8dd602f13cb7572fa4e224d59b0ef5e
Eline Madsø <elinemadso@gmail.com>
2023-11-17 16:27:21 +0100

    started on context

--- Prosjektmapper/frontend/src/context/Formula1Context.tsx
+++ Prosjektmapper/frontend/src/context/Formula1Context.tsx
@@ -1,4 +1,19 @@
-//import {useState, createContext} from 'react'
+import {createContext, useContext, useState, useEffect} from 'react'
+import axios from 'axios';
 
-//export const Formula1Context = createContext();
+export const Formula1Context = createContext();
+
+export const useFormula1Context = () => {
+    return useContext(Formula1Context);
+}
+
+export const Formula1Provider = ({ children }) => {
+    const [drivers, setDrivers] = useState([]);
+    const [teams, setTeams] = useState([]);
+    const [races, setRaces] = useState([]);
+
+    useEffect(() => {
+
+    }, []);
+}
 
