6c05181a05aa554176668955b052778c181bfc33
Eline Madsø <elinemadso@gmail.com>
2023-11-17 20:18:19 +0100

    making navigationbar

--- Prosjektmapper/frontend/src/App.tsx
+++ Prosjektmapper/frontend/src/App.tsx
@@ -1,18 +1,36 @@
 import { BrowserRouter, Routes, Route,Link } from 'react-router-dom'
 import { HomePage, DriverPage, NewDriverPage, QuizPage } from './pages'
+import './app.css';
+import homeIcon from './assets/New_era_F1_logo.png';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <nav>
-          <ul><li></li></ul>
-          <ul><li></li></ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top navbar-fixed-top">
+          <Link className="navbar-brand" to="/"><img src={homeIcon} alt="Home" width="110em" className="d-inline-block align-top"/></Link>
+
+          <div className="collaps navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto" id="navbarStyle">
+              <li className="nav-item">
+                <Link className="nav-link" to="/drivers">SEASONS FORMULA 1 DRIVERS AND TEAMS</Link>
+              </li>
+              <li className="nav-item ml-2">
+                <Link className="nav-link" to="/new-driver">SIGN UP AS A DRIVER</Link>
+              </li>
+              <li className="nav-item ml-1">
+                <Link className="nav-link" to="/quiz">QUIZ</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
         <main>
           <Routes>
-            <Route></Route>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/drivers" element={<DriverPage/>} />
+            <Route path="/new-driver" element={<NewDriverPage/>} />
+            <Route path="/quiz" element={<QuizPage/>} />
           </Routes>
         </main>
       </BrowserRouter>
--- Prosjektmapper/frontend/src/pages/DriverPage.jsx
+++ Prosjektmapper/frontend/src/pages/DriverPage.jsx
@@ -1,7 +1,7 @@
 const DriverPage = () => {
     return (
         <section>
-
+            <h1>F1 DRIVERS 2023</h1>
         </section>
     )
 }
