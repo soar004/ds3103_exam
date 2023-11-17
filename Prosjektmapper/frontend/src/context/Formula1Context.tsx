ff36479bebc4c92ba6faf8bad6a9ab75c19e7ec5
Eline Madsø <elinemadso@gmail.com>
2023-11-17 18:20:05 +0100

    adding components, figuring out context, starting on services

--- Prosjektmapper/frontend/src/context/Formula1Context.tsx
+++ Prosjektmapper/frontend/src/context/Formula1Context.tsx
@@ -1,4 +1,4 @@
-import {createContext, useContext, useState, useEffect} from 'react'
+/*import {createContext, useContext, useState, useEffect} from 'react'
 import axios from 'axios';
 
 export const Formula1Context = createContext();
@@ -6,14 +6,50 @@ export const Formula1Context = createContext();
 export const useFormula1Context = () => {
     return useContext(Formula1Context);
 }
-
 export const Formula1Provider = ({ children }) => {
-    const [drivers, setDrivers] = useState([]);
-    const [teams, setTeams] = useState([]);
-    const [races, setRaces] = useState([]);
+    const [driver, setDriver] = useState([]);
+    const [team, setTeam] = useState([]);
+    const [race, setRace] = useState([]);
+
+    const getDriver = async () => {
+        try{
+            //urlen til api drivers
+            const result = await axios.get();
+            setDriver(result.data);
+        }catch(error){ 
+            console.log("Error getting drivers:", error);
+        }
+    }
+
+    const getTeam = async () => {
+        try{
+            //urlen til api teams
+            const result = await axios.get();
+            setTeam(result.data);
+        }catch(error){ 
+            console.log("Error getting teams:", error);
+        }
+    }
+
+    const getRace = async () => {
+        try{
+            //urlen til api races
+            const result = await axios.get();
+            setRace(result.data);
+        }catch(error){ 
+            console.log("Error getting races:", error);
+        }
+    }
 
     useEffect(() => {
-
+        getDriver();
+        getTeam();
+        getRace();
     }, []);
+    return (
+        <Formula1Context.Provider value={{driver, team, race, getDriver, getTeam, getRace}}>
+            {children}
+        </Formula1Context.Provider>
+    )
 }
-
+*/
