adf3c84c92ac821b6819ca555f7e0feda4666bd5
Solveig Årvik <soar004@student.kristiania.no>
2023-11-20 18:35:35 +0100

    Forslag til ny struktur på js

--- Prosjektmapper/Formula1API/wwwroot/scripts/index.js
+++ Prosjektmapper/Formula1API/wwwroot/scripts/index.js
@@ -1,112 +1,66 @@
-//html-objects
 const outputSection = document.querySelector("#output-section");
-const getAllDriversBtn = document.querySelector("#get-all-drivers-btn");
-const SaveNewDriverBtn = document.querySelector("#save-new-driver-btn");
-const deleteDriverBtn = document.querySelector("#delete-driver-btn");
-const editDriverBtn = document.querySelector("#edit-driver-btn");
 
-const getAllTeamsbtn = document.querySelector("#get-all-teams-btn");
-const getAllRacesBtn = document.querySelector("#get-all-races-btn");
+const fetchData = async (url, displayFunction) => {
+	try {
+		const result = await axios.get(url);
+		clearOutput();
+		displayFunction(result.data);
+	} catch (err) {
+		console.log(err);
+	}
+};
 
- const getAllDrivers = async () => {
-    try{
-        const result = await axios.get("http://localhost:3000/api/drivers");
-        console.log(result);
-    }
-    catch(err){
-        console.log(err);
-    }
- }
+const displayDrivers = (drivers) => {
+	drivers.forEach((driver) => showDriver(driver));
+};
 
- const getAllTeams = async () => {
-    try{
-        const result = await axios.get("http://localhost:3000/api/teams");
-        console.log(result);
-    }
-    catch(err){
-        console.log(err);
-    }
- }
+const displayTeams = (teams) => {
+	teams.forEach((team) => showTeam(team));
+};
 
- const getAllRaces = async () => {
-    try{
-        const result = await axios.get("http://localhost:3000/api/races");
-        console.log(result);
-    }
-    catch(err){
-        console.log(err);
-    }
- }
+const displayRaces = (races) => {
+	races.forEach((race) => showRace(race));
+};
 
- //funksjoner
+const clearOutput = () => {
+	outputSection.innerHTML = "";
+};
 
- const showDrivers = (driverArray) => {
-    driverArray.forEach(driver => {
-        showDriver(driver);
-    });
- }
- const showTeams = (teamArray) => {
-    teamArray.forEach(team => {
-        showTeam(team);
-    });
- }
- const showRaces = (raceArray) => {
-    raceArray.forEach(race => {
-        showRace(race);
-    });
- }
+const showDriver = (driver) => {
+	// display driver details
+};
 
- const showDriver = (driver) => {
-    let htmlTxt = `
-        <article>
-            <h3>${driver.Firstname}${driver.lastname}(${driver.Id})</h3>
-            <p>${driver.DateOfBirth}</p>
-            <p>${driver.Nationality}</p>
-            <img>
-            <p>${driver.Age} år gammel </p>
-        </article>
-    `;
-    outputSection.innerHTML += htmlTxt;
- }
+const showTeam = (team) => {
+	// display team details
+};
 
- const showTeam = (team) => {
-    let htmlTxt = `
-        <article>
-            <h3>${team.Manufacturer}(${team.Id})</h3>
-            <p>${team.Driver1}(${team.Driver1Id}) ${team.Driver2} (${team.Driver2Id})</p>
-            <img>
-        </article>
-    `;
-    outputSection.innerHTML += htmlTxt;
- }
+const showRace = (race) => {
+	// display race details
+};
 
- const showRace = (race) => {
-    let htmlTxt = `
-        <article>
-            <h3>${race.Grandprix}</h3>
-            <p>${race.Id}
-            <p>${race.Winner} ${race.WinnerTime}</p>
-            <p>${race.NumberOfLaps}</p>
-        </article>
-    `;
-    outputSection.innerHTML += htmlTxt;
- }
- const clearOutput = () => {
-    outputSection.innerHTML = "";
- }
+// Event listeners
+const buttons = {
+	"get-all-drivers-btn": {
+		url: "http://localhost:3000/api/drivers",
+		displayFunc: displayDrivers,
+	},
+	"get-all-teams-btn": {
+		url: "http://localhost:3000/api/teams",
+		displayFunc: displayTeams,
+	},
+	"get-all-races-btn": {
+		url: "http://localhost:3000/api/races",
+		displayFunc: displayRaces,
+	},
+	// Add more buttons here as needed
+};
 
-//events
-
-getAllDriversBtn.addEventListener("click", () => {
-    clearOutput();
-    getAllDrivers();
+Object.keys(buttons).forEach((buttonId) => {
+	const button = document.querySelector(`#${buttonId}`);
+	if (button) {
+		button.addEventListener("click", async () => {
+			const { url, displayFunc } = buttons[buttonId];
+			await fetchData(url, displayFunc);
+		});
+	}
 });
-getAllTeamsbtn.addEventListener("click", () => {
-    clearOutput();
-    getAllTeams();
-})
-
-getAllRacesBtn.addEventListener("click", () => {
-    clearOutput();
-    getAllRaces();
-})
