85e586ab45075f291ce076d04fc96f21be627121
Eline Madsø <elinemadso@gmail.com>
2023-11-24 16:39:15 +0100

    fikset databasen til drivers

--- Prosjektmapper/Formula1API/Models/Driver.cs
+++ Prosjektmapper/Formula1API/Models/Driver.cs
@@ -8,23 +8,10 @@ namespace Formula1API.Models
         public int? Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public int? Age {get; set;}
         public string? Nationality { get; set; }
         public byte[]? ImgDriver { get; set; }
 
         // Calculate property for age
-        public int Age { get => CalculateAge(); set => throw new NotImplementedException();}
-        
-        private int CalculateAge()
-        {
-            // Calculate the driver's age based on their DOB and current date
-            DateTime currentDate = DateTime.Now;
-            int age = currentDate.Year - DateOfBirth.Year;
-            if (currentDate.Month < DateOfBirth.Month || (currentDate.Month == DateOfBirth.Month && currentDate.Day < DateOfBirth.Day))
-            {
-                age--;
-            }
-            return age;
-        }
     }
 }
